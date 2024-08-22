a5728bb70e3ec800a071f9f896d704b758eb91da
Favour Ajor <pretty.ajor@gmail.com>
2024-08-22 01:31:16 +0100

    Added the weather forecast to the Javascript

--- src/index.js
+++ src/index.js
@@ -55,6 +55,11 @@ function handleSearch(event) {
 
   searchCity(searchInput.value);
 }
+function formattedDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "dab41bf0ae0cb164587f31o31483a6et";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -62,21 +67,25 @@ function getForecast(city) {
 }
 function searchForecast(response) {
   console.log(response.data);
-  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
+
   let forecastHTML = "";
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formattedDay(day.time)}</div>
+            <img src="${day.condition.icon_url}"class="weather-forecast-icon"/>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>15°</strong>
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
               </div>
-              <div class="weather-forecast-temperature">13°</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°</div>
             </div>
           </div>`;
+    }
   });
   let forecast = document.querySelector("#forecast");
   forecast.innerHTML = forecastHTML;
@@ -85,4 +94,4 @@ function searchForecast(response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
 
-searchCity("Paris");
+searchCity("Lagos");
